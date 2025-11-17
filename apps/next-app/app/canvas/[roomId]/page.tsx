77c37b847eef3ba3db3128e26320deefa0c7dbdc
Karmanay Sharma <karmanaysharmaog@gmail.com>
2025-11-18 03:05:28 +0530

    rect init

---
+++ apps/next-app/app/canvas/[roomId]/page.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { useEffect, useRef } from "react"
+
+export default function Canvas() {
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+
+    useEffect(() => {
+
+        if (canvasRef.current) {
+            const canvas = canvasRef.current
+            const ctx = canvas.getContext("2d")
+            if (!ctx) return
+
+            let mouseDown = false
+            const coordinates = {
+                x:0,
+                y:0,
+                w:0,
+                h:0
+            }
+            
+            canvas.addEventListener("mousedown",(e) => {
+                console.log("mousedown")
+                mouseDown = true
+                coordinates.x = e.clientX
+                coordinates.y = e.clientY
+            })
+
+            canvas.addEventListener("mousemove",(e) => {
+                if (mouseDown) {
+                    coordinates.w = e.clientX - coordinates.x
+                    coordinates.h = e.clientY - coordinates.y
+                    ctx.clearRect(0,0,canvas.width,canvas.height)
+                    ctx.strokeRect(coordinates.x,coordinates.y,coordinates.w,coordinates.h)  
+                }   
+            })
+
+            canvas.addEventListener("mouseup",(e) => {
+                console.log("mouseup")
+                mouseDown = false
+            })
+
+        }
+
+    },[canvasRef])
+
+    return <div>
+        <canvas ref={canvasRef} width={1000} height={500} className="bg-green-300" ></canvas>
+    </div>
+}
